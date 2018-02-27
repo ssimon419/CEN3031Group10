40cf2e5706cb6be7f4fa83486152d7033a37704b
jarnold999 <jedidiaharnold9@gmail.com>
2018-02-27 12:30:39 -0500

    Sprint 1 End
    added portal gun & accompanying graphics
    sprint 1 end

---
+++ Portal 2D/Assets/scripts/portal_gun.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal_gun : MonoBehaviour {
+
+	public float fireRate = 0;
+	public LayerMask whatToHit;
+
+	public Transform portal1;
+	public Transform portal2;
+	public Transform BulletTrailPrefab;
+
+	public float effectSpawnRate = 10;
+	float timeToSpawnEffect = 0;
+	float timeToFire = 0;
+	Transform firePoint;
+
+	// Use this for initialization
+	void Awake  () {
+		firePoint = transform.Find("FirePoint");
+		if (firePoint == null)
+		{
+			Debug.LogError("No firepoint?");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//  Shoot();
+		if (fireRate == 0)
+		{
+			if (Input.GetButtonDown("Fire1"))
+			{
+				Shoot();
+			}
+			else if (Input.GetButtonDown("Fire2"))
+			{
+				Shoot2();
+			}
+		}
+		else
+		{
+			if (Input.GetButton("Fire1") && Time.time > timeToFire)
+			{
+				timeToFire = Time.time + 1 / fireRate;
+				Shoot();
+			}
+			else if (Input.GetButton("Fire2") && Time.time > timeToFire)
+			{
+				timeToFire = Time.time + 1 / fireRate;
+				Shoot2();
+			}
+		}
+	}
+
+	void Shoot ()
+	{
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+		if (Time.time >= timeToSpawnEffect)
+		{
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
+		Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition)*100);
+
+		if (hit.collider != null)
+		{
+			Debug.DrawLine(firePointPosition, hit.point, Color.red);
+			Debug.Log("We hit" + hit.collider.name);
+			portal1.position = hit.point;
+		}
+	}
+
+	void Shoot2()
+	{
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+		if (Time.time >= timeToSpawnEffect)
+		{
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
+		Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100);
+
+		if (hit.collider != null)
+		{
+			Debug.DrawLine(firePointPosition, hit.point, Color.red);
+			Debug.Log("We hit" + hit.collider.name);
+			portal2.position = hit.point;
+		}
+	}
+}
