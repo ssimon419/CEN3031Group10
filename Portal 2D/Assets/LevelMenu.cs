95761b8c3a9bb48616be3ee2d329eaa5d0727734
Tolar5515 <bmgropper@gmail.com>
2018-04-17 05:19:00 -0400

    Level Select Menu
    Added Level Select Menu with option to play "Level 0 - Testing Level"

---
+++ Portal 2D/Assets/LevelMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour {
+	public void SetLevel (int scene){
+		SceneManager.LoadScene(scene);
+	}
+}
---
+++ Portal 2D/Assets/SettingsMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+	public Slider volumeSlider;
+
+	public void VolumeControl(){
+		AudioListener.volume = volumeSlider.value;
+	}
+}
